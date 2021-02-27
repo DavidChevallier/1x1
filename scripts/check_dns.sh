6487a1350d0ce5268acf8e698dd4ccb1a64eaa5d
David Halbani <david@halbani.de>
2021-02-27 12:36:05 +0100

    add mini dns Check script

---
+++ scripts/check_dns.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#################################
+##  Check DNS Records from
+##      "gg.Amcha.co.il"
+##
+#################################
+
+dig gg.amcha.co.il +noall +answer
